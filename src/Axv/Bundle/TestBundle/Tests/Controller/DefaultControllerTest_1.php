ca28cd2d1425ac0d3a0cd28c21958476c01c8df2
Miguel DC <miguel@axevision.fr>
2015-06-16 16:25:40 +0200

    Helper Added

---
+++ src/Axv/Bundle/TestBundle/Tests/Controller/DefaultControllerTest_1.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Axv\Bundle\TestBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+    
+    public function testFeature2()
+    {
+        
+        $this->assertTrue(true);
+    }
+}
